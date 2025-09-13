b83f65089a7474f5a12bcf071ec49071749aa1a4
Isanz905 <ignaciosanz511@gmail.com>
2025-09-13 17:11:23 +0200

    Ejercicios del 1 al 7

--- Ejercicios1/src/Ejercicios.java
+++ Ejercicios1/src/Ejercicios.java
@@ -1,15 +1,20 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.channels.FileLockInterruptionException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 public class Ejercicios {
 
 
-    /*1. Crea un método listarDirectorio() que devuelva una array con el listado del
+    /*1. Crea un metodo listarDirectorio() que devuelva una array con el listado del
          contenido (archivos y carpetas) del directorio actual.
-        ¿Este método debería ser dinámico o estático? ¿por qué?*/
+        ¿Este metodo debería ser dinámico o estático? ¿por qué?*/
 
+    // en este caso estatico porque no depende de un objeto para funcionar
     public void listarDirectorio(){
 
         //Declaramos el objeto file
@@ -29,7 +34,7 @@ public class Ejercicios {
         }
     }
 
-    /*2. Crea un método listarDirectorio(String directorio) que devuelva una
+    /*2. Crea un metodo listarDirectorio(String directorio) que devuelva una
          array con el listado del contenido del directorio indicado como argumento siempre y cuando este sea un directorio
          y no un archivo. Pruébalo pasándole al menos
          una ruta absoluta y una relativa.
@@ -42,7 +47,7 @@ public class Ejercicios {
 
         // comprobamos si la ruta existe o si no es un directorio
         if(!ruta.exists() || !ruta.isDirectory()){
-            System.out.println("Error en la ruta");
+            System.err.println("Error en la ruta");
         }
         //declaracion del array donde se guarda la informacion obtenida y la imprimimos
         String [] directorios = ruta.list();
@@ -52,25 +57,169 @@ public class Ejercicios {
         }
     }
 
-    // Crea un método existeFichero(String directorio, String fichero) que
+    // Crea un metodo existeFichero(String directorio, String fichero) que
     //compruebe si existe dicho fichero en el directorio indicado.
 
      public void existeFichero(String directorio, String fichero){
 
-        File rutaCompleta =  new File(directorio);
-        if(!rutaCompleta.exists()){
-            System.out.println("Directorio no encontrado");
-        }
+        // creamos la ruta con el directorio y el fichero pasados por parametros
+        File rutaCompleta =  new File(directorio, fichero);
 
-        File rutaConArchivo = new File(directorio + "/" + fichero);
-        if(rutaConArchivo.exists()){
-            System.out.println("Existe");
-        }
+        //comprobamos con el metodo exists si el archivo existe en la ruta que nos han mandado
+
+         if (rutaCompleta.exists()) {
+             System.out.println("Fichero existe");
+         } else {
+             System.out.println("Fichero no existe");
+         }
+
 
 
      }
 
+     //4. Crea un metodo generarArchivo que a partir de una ruta que se le pase como
+    //argumento, cree un archivo txt con nombre TunombreTuapellido en la ruta en la
+    //que se le ha proporcionado. Presta atención a los posibles errores que puedan
+    //darse.
+    //¿Qué pasa si la ruta no existe? ¿puedes solucionarlo?
 
+
+    public void generarArchivo()  {
+        // declaro un escaner por el que se introducira el directorio
+        Scanner sc = new Scanner(System.in);
+        String directorio = "";
+        boolean escrito = false;
+
+        // con el bucle compruebo que solo me pasan un directorio
+        do{
+            System.out.println("Escribe un directorio: ");
+            directorio = sc.nextLine();
+            File ruta = new File(directorio);
+            // comprobacion de si es un directorio para salir del bucle y crear el fichero
+
+            // debido a posibles erroes de ejecicion como permisos o fallos en el sistema de archivos hay que meterlo en un try catch para manejar las posbiles excepciones
+            if(ruta.isDirectory()){
+                try{
+                    File fichero = new File(ruta, "IgnacioSanz.txt");
+                    fichero.createNewFile();
+                    escrito = true;
+                }catch(IOException ioe){
+                    ioe.printStackTrace();
+                }
+
+            }else{
+                System.out.println("No es un directorio");
+            }
+
+
+        }while(escrito==false);
+    }
+    //5. Crea un metodo renombrarArchivo que coja un archivo cuyo path absoluto se
+    //le pase como argumento y lo renombre añadiendo delante DAM2.
+    //Pruébalo con el archivo creado en el ejercicio anterior.
+    //El archivo antiguo, ¿desaparece? se sobreEscribe
+
+    public void renombrarArchivo(String directorio){
+
+        // recogemos la ruta y declaramos el objeto
+        File ruta = new File(directorio);
+        if(!ruta.exists()){
+            System.err.println("Error en la ruta");
+        }
+        try{
+            //declaramos el nuevo nombre del archivo
+            File nuevoNombre = new File(ruta.getParent(),"IgnacioSanzDAM2.txt");
+
+            if(nuevoNombre.exists()){
+                System.out.println("Nombre existe");
+            }
+            // si devuelve true, que se ha renombrado, se imprimirá el mensaje de exito
+            boolean exito = ruta.renameTo(nuevoNombre);
+            if(exito){
+                System.out.println("Renombrado exitosamente");
+            }
+        }catch(SecurityException se){
+            se.printStackTrace();
+        }catch (NullPointerException npe){
+            npe.printStackTrace();
+        }
+
+    }
+
+    // 6. Crea un metodo que se llame borrarArchivo que reciba un path absoluto y
+    //elimine el archivo indicado.
+    //Pruébalo con el archivo del ejercicio anterior.
+    //En la clase File hay métodos para cambiar los atributos del archivo. Prueba a
+    //modificar el metodo haciendo el archivo de solo lectura antes de eliminarlo. ¿Qué
+    //sucede? ¿por qué?
+
+    public void borrarArchivo(String directorio){
+
+        File ruta = new File(directorio);
+
+        try{
+            if(!ruta.exists()){
+                throw new FileNotFoundException("El archivo no existe.");
+
+            }
+            boolean lectura = ruta.setReadable(true);
+            if(lectura){
+                System.out.println("Permiso cambiado");
+            }
+
+            boolean eliminado = ruta.delete();
+            if(eliminado){
+                System.out.println("Eliminado exitosamente");
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (SecurityException se){ // esta se hace por si falla la ejecucion
+            se.printStackTrace();
+        }catch (NullPointerException npe){
+            npe.printStackTrace();
+        }
+
+
+
+    }
+    // 7. Crea un metodo eliminarDirectorio que reciba una ruta y elimine el directorio
+    //indicado por ella.
+    //¿Elimina directorios con contenido? ¿cómo se puede solucionar?
+    //Modifica el metodo para que elimine directorios que contengan solo archivos o
+    //estén vacíos e indique que no puede hacerlo si contienen otros directorios.
+
+    public void eliminarDirectorio(String directorio){
+        File ruta = new File(directorio);
+            try{
+                if(!ruta.exists()){
+                    throw new FileNotFoundException("El directorio no existe.");
+                }
+                // listamos los archivos dentro del directorio
+                File[] ficheros = ruta.listFiles();
+
+                // este bucle for recorre el arreglo y llama al metodo de forma recursiva para eliminar los archivos dentro del directorio
+                for(int i = 0; i < ficheros.length; i++){
+                    if(ficheros[i].isDirectory()){
+                        eliminarDirectorio(ficheros[i].getAbsolutePath());
+                    }
+
+                    ficheros[i].delete();
+                }
+                // elimina el directorio
+                boolean eliminado = ruta.delete();
+                if(eliminado){
+                    System.out.println("Eliminado exitosamente");
+                }
+
+            }catch (FileNotFoundException e){
+                System.out.println(e.getMessage());
+            }catch (SecurityException se){ // esta se hace por si falla la ejecucion
+                se.printStackTrace();
+            }catch (NullPointerException npe){
+                npe.printStackTrace();
+            }
+
+    }
 
 
 }
--- Ejercicios1/src/Main.java
+++ Ejercicios1/src/Main.java
@@ -1,8 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
-    public static void main(String[] args) {
-       Ejercicios ejercicios = new Ejercicios();
-       ejercicios.existeFichero("C:\\pruebaAccesoDatos", "prueba.txt");
+    public static void main(String[] args)  {
+
     }
 }
