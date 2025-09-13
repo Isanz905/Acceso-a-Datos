42d362c6aef9e075ffcc2ee3f24469680f3d35f7
Isanz905 <ignaciosanz511@gmail.com>
2025-09-13 17:34:01 +0200

    Ejercicios 8 terminado

--- Ejercicios1/src/Ejercicio8.java
+++ Ejercicios1/src/Ejercicio8.java
@@ -1,2 +1,24 @@
-public class Ejercicio8 {
+import java.io.File;
+import java.util.Arrays;
+
+public class Ejercicio8 extends File {
+
+
+    public Ejercicio8(String pathname) {
+        super(pathname);
+    }
+
+    public String showInfo() {
+
+        return "Nombre: " + getName() + "\n" +
+                "Ruta: " + getPath() + "\n" +
+                "Ruta absoluta: " + getAbsolutePath() + "\n" +
+                "¿Se puede leer? " + canRead() + "\n" +
+                "¿Se puede escribir? " + canWrite() + "\n" +
+                "Tamaño: " + length() + "\n" +
+                "¿Es un directorio? " + isDirectory() + "\n" +
+                "Contenidos: " + Arrays.toString(list()) + "\n" +
+                "¿Es un fichero?" + isFile();
+
+    }
 }
--- Ejercicios1/src/Main.java
+++ Ejercicios1/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args)  {
-
+        Ejercicio8 ej = new Ejercicio8("C:\\xampp");
+        System.out.println(ej.showInfo());
     }
 }
