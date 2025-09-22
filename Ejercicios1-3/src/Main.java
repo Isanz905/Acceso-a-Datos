362fe13984b8ca5a12ebce8b5e7761604766d0c0
Isanz905 <ignaciosanz511@gmail.com>
2025-09-22 18:41:11 +0200

    ejercicios terminados

--- Ejercicios1-3/src/Main.java
+++ Ejercicios1-3/src/Main.java
@@ -1,12 +1,17 @@
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ArchivoTXT arch = new ArchivoTXT("C:/Users/ignac/Documents/Lorem ipsum.txt");
+        // ArchivoTXT arch = new ArchivoTXT("C:/Users/ignac/Documents/Lorem ipsum.txt");
         // System.out.println(arch.aVerso());
         // arch.codifica("C:/Users/ignac/Documents/nuevo.txt");
         // arch.codificaBuffer("C:/Users/ignac/Documents/nuevo.txt");
-         arch.codificaFiles("C:/Users/ignac/Documents/nuevo.txt");
+        // arch.codificaFiles("C:/Users/ignac/Documents/nuevo.txt");
         // arch.mover("C:/Users/ignac/Documents/pruebas/Lorem ipsum.txt");
         // System.out.println(arch.contar());
         // System.out.println(arch.contarLetras());
@@ -16,6 +21,27 @@ public class Main {
         // System.out.println(arch.cuentaVocales("C:/Users/ignac/Documents/numVocales.txt"));
         // System.out.println(arch.cuentaVocales2("C:/Users/ignac/Documents/numVocales.txt"));
         // arch.frecuenciaLetras();
+
+        //--------------------------------------------Ejercicio 11-------------------------------------------------------------------
+
+       /** List<Personaje> personajes = new ArrayList<>();
+        personajes.add(new Personaje("pepe", "martillo", 35, 80.0, true));
+        personajes.add(new Personaje("ana", "espada", 28, 70.5, false));
+        personajes.add(new Personaje("luis", "arco", 40, 65.0, true));
+        personajes.add(new Personaje("maria", "lanza", 22, 55.3, false));
+        personajes.add(new Personaje("carlos", "hacha", 37, 82.7, true));
+        personajes.add(new Personaje("sofia", "baston", 30, 60.0, true));
+
+        Personaje pj = new Personaje();
+        pj.toCSV(personajes, "personas.csv");*/
+
+        // Personaje persona = new Personaje("Fernando", "maza", 40, 100.0, false);
+        // persona.escribirFinalCSV(persona,"personas.csv");
+
+        Personaje pj = new Personaje();
+
+        System.out.println(pj.leerCSV("personas.csv"));
+
 
     }
 }
--- Ejercicios1-3/src/Personaje.java
+++ Ejercicios1-3/src/Personaje.java
@@ -1,16 +1,21 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Personaje {
 
     private String nombre;
     private String herramienta;
     private int edad;
-    private float peso;
+    private double peso;
     private boolean usado;
 
-    public Personaje(String nombre, String herramienta, int edad, float peso, boolean usado) {
-        this.nombre = nombre;
-        this.herramienta = herramienta;
-        this.edad = edad;
-        this.peso = peso;
+    public Personaje(String nombre, String herramienta, int edad, double peso, boolean usado) {
+        setNombre(nombre);
+        setHerramienta(herramienta);
+        setEdad(edad);
+        setPeso(peso);
         this.usado = usado;
     }
 
@@ -22,7 +27,11 @@ public class Personaje {
     }
 
     public void setNombre(String nombre) {
-        this.nombre = nombre;
+        if(nombre == null){
+            throw new IllegalArgumentException("El nombre es obligatorio");
+        }else{
+            this.nombre = nombre;
+        }
     }
 
     public String getHerramienta() {
@@ -30,7 +39,12 @@ public class Personaje {
     }
 
     public void setHerramienta(String herramienta) {
-        this.herramienta = herramienta;
+        if(herramienta == null){
+            throw new IllegalArgumentException("La herramienta es obligatorio");
+        }else{
+            this.herramienta = herramienta;
+        }
+
     }
 
     public int getEdad() {
@@ -38,15 +52,25 @@ public class Personaje {
     }
 
     public void setEdad(int edad) {
-        this.edad = edad;
+        if(edad <= 0){
+            throw new IllegalArgumentException("El edad no puede ser menor a 0");
+        }else{
+            this.edad = edad;
+        }
+
     }
 
-    public float getPeso() {
+    public double getPeso() {
         return peso;
+
     }
 
-    public void setPeso(float peso) {
-        this.peso = peso;
+    public void setPeso(double peso) {
+        if(peso <= 0){
+            throw new IllegalArgumentException("El peso no puede ser menor a 0");
+        }else{
+            this.peso = peso;
+        }
     }
 
     public boolean isUsado() {
@@ -56,4 +80,75 @@ public class Personaje {
     public void setUsado(boolean usado) {
         this.usado = usado;
     }
+
+    public void toCSV(List<Personaje> personajes, String archivoCSV) {
+        File file = new File(archivoCSV);
+        try (FileWriter fw = new FileWriter(file, true);) {
+            // Escribir cada personaje
+            for (Personaje personaje : personajes) {
+                String[] datos = {
+                        personaje.getNombre(),
+                        personaje.getHerramienta(),
+                        String.valueOf(personaje.getEdad()),
+                        String.valueOf(personaje.getPeso()),
+                        String.valueOf(personaje.isUsado())
+                };
+                fw.write(String.join(",", datos) + "\n");
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void escribirFinalCSV(Personaje personaje, String archivoCSV) {
+        File file = new File(archivoCSV);
+        try(FileWriter fw = new FileWriter(file, true)){
+            String[] datos = {
+                    personaje.getNombre(),
+                    personaje.getHerramienta(),
+                    String.valueOf(personaje.getEdad()),
+                    String.valueOf(personaje.getPeso()),
+                    String.valueOf(personaje.isUsado())
+
+            };
+            fw.write(String.join(",", datos) + "\n");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Personaje> leerCSV(String archivoCSV) {
+        List<Personaje> personajes = new ArrayList<>();
+        String linea;
+
+        File file = new File(archivoCSV);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+
+            while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(",");
+
+                String nombre = datos[0];
+                String herramienta = datos[1];
+                int edad = Integer.parseInt(datos[2]);
+                double peso = Double.parseDouble(datos[3]);
+                boolean usado = Boolean.parseBoolean(datos[4]);
+
+                personajes.add(new Personaje(nombre, herramienta, edad, peso, usado));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return personajes;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + " \n " + "Herramienta: "+ herramienta + " \n " + "Edad: " + edad + " \n "+"Peso: " + peso + "\n" + "Usado: " + usado;
+    }
+
+
 }
